b2472cdc696df291b0e5bcd347841e31a1f09102
harshit-506 <supriyamohapatra658@gmail.com>
2023-12-30 20:44:00 +0530

    done with pagination

--- pagination/frontend/src/App.js
+++ pagination/frontend/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page,setPage]=useState(1) //by default
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const getApi = async () => {
+      try {
+        const response = await axios.get("https://dummyjson.com/products", {
+          cancelToken: source.token,
+        });
+        setProducts(response.data.products); // Assuming the API response is an array of products directly
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled:", error.message);
+        } else {
+          console.log("Error:", error.message);
+        }
+      }
+    };
+
+    getApi();
+
+    return () => {
+      // Cancel the request when the component unmounts or before re-running the effect
+      source.cancel("Cleanup: Component unmounted");
+    };
+  }, []);
+
+const selectPageHandler=(selectedPage)=>{
+  if(selectedPage >=1 && selectedPage<=products.length/3 && selectedPage!==page){
+    setPage(selectedPage)
+  }
+ 
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page,page*10).map((item) => (
+            <span className="products__single" key={item.id}>
+              <img src={item.thumbnail} alt={item.title} />
+              <span>{item.title}</span>
+            </span>
+      ))}
+        </div>
+  )}
+  {/* now pagination start */}
+  {products.length > 0 && <div className="pagination">
+   <span className={page > products.length/10 ? "products__disabled":""} onClick={()=>setPage(page-1)}><GrLinkPrevious/></span>
+   {[...Array(products.length/3)].map((_,i)=>{
+    return <span 
+    className={page === i+1 ? "page__selected": ""}
+    onClick={()=>selectPageHandler(i+1)} key={i}>{i+1}</span>
+   })}
+   
+   <span className={page < products.length/10 ? "products__disabled":""} onClick={()=>setPage(page+1)}><GrLinkNext/></span>
+    </div>
+}
     </div>
   );
 }
