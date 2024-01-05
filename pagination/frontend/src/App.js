f12adfcb2ef300c61e44785c00a2e61d73479b0a
harshit-506 <supriyamohapatra658@gmail.com>
2024-01-05 12:53:37 +0530

    done with pagination

--- pagination/frontend/src/App.js
+++ pagination/frontend/src/App.js
@@ -5,7 +5,7 @@ import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [page,setPage]=useState(1) //by default
+  const [page, setPage] = useState(1); //by default
 
   useEffect(() => {
     const source = axios.CancelToken.source();
@@ -33,37 +33,56 @@ function App() {
     };
   }, []);
 
-const selectPageHandler=(selectedPage)=>{
-  if(selectedPage >=1 && selectedPage<=products.length/3 && selectedPage!==page){
-    setPage(selectedPage)
-  }
- 
-}
+  // Define variables for pagination
+  const productPerPage = 10;
+  const totalPages = Math.ceil(products.length / productPerPage);
+
+  // Logic to handle visible products on the current page
+  const startIndex = (page - 1) * productPerPage;
+  const endIndex = startIndex + productPerPage;
+  const visibleProducts = products.slice(startIndex, endIndex);
 
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.slice(page,page*10).map((item) => (
-            <span className="products__single" key={item.id}>
+          {visibleProducts.map((item, index) => (
+            <span className="products__single" key={index}>
               <img src={item.thumbnail} alt={item.title} />
               <span>{item.title}</span>
             </span>
-      ))}
+          ))}
         </div>
-  )}
-  {/* now pagination start */}
-  {products.length > 0 && <div className="pagination">
-   <span className={page > products.length/10 ? "products__disabled":""} onClick={()=>setPage(page-1)}><GrLinkPrevious/></span>
-   {[...Array(products.length/3)].map((_,i)=>{
-    return <span 
-    className={page === i+1 ? "page__selected": ""}
-    onClick={()=>selectPageHandler(i+1)} key={i}>{i+1}</span>
-   })}
-   
-   <span className={page < products.length/10 ? "products__disabled":""} onClick={()=>setPage(page+1)}><GrLinkNext/></span>
-    </div>
-}
+      )}
+
+      {/* Pagination */}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span
+            className={page === 1 ? "products__disabled" : ""}
+            onClick={() => setPage(page - 1)}
+          >
+            <GrLinkPrevious />
+          </span>
+
+          {[...Array(totalPages)].map((_, i) => (
+            <span
+              className={page === i + 1 ? "page__selected" : ""}
+              onClick={() => setPage(i + 1)}
+              key={i}
+            >
+              {i + 1}
+            </span>
+          ))}
+
+          <span
+            className={page === totalPages ? "products__disabled" : ""}
+            onClick={() => setPage(page + 1)}
+          >
+            <GrLinkNext />
+          </span>
+        </div>
+      )}
     </div>
   );
 }
